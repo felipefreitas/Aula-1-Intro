5890d63baf13d21e702a01d978fae026c106be01
Felipe Freitas <felipesafreitas@gmail.com>
2018-08-07 10:32:52 -0300

    added parallel form for image processing

--- grayscale/student.cpp
+++ grayscale/student.cpp
@@ -8,5 +8,19 @@ void process_image_parallel(struct rgb *input_image,
     size_t rows,
     size_t cols)
 {
-    // your code goes here
+	#pragma omp parallel for
+    for(size_t r = 0; r < rows; r++)
+    {
+        for(size_t c = 0; c < cols; c++)
+        {
+            size_t index = r * cols + c;
+            struct rgb pixel = input_image[index];
+
+            float channel = .299f * pixel.red 
+                + .587f * pixel.green
+                + .114f * pixel.blue;
+
+            output_image[index] = channel;
+        }
+    }
 }
