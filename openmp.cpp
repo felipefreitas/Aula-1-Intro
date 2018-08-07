2a17c90dbe96d9c3b5b032ec7e71d1e43f076413
Felipe Freitas <felipesafreitas@gmail.com>
2018-08-07 10:29:51 -0300

    ADDING second exercise"

---
+++ openmp.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+    omp_set_num_threads(2);
+
+    #pragma omp parallel for
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Thread ID: %d -- index: %d\n",
+            omp_get_thread_num(), i);
+    }
+}
